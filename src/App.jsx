541771888f6675af692e9f1a8d445917b292467f
Manav <manavpatil469@gmail.com>
2025-08-06 15:22:15 +0530

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-
-import './App.css'
+import { Analytics } from '@vercel/analytics/react';
+import './App.css';
 import App2 from './Dashboard';
 
 function App() {
-  return <App2/>
- 
+  return (
+    <>
+      <App2 />
+      <Analytics />
+    </>
+  );
 }
 
-export default App
+export default App;
